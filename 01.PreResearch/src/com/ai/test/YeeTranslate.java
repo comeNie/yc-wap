1576a33a8469ecb7f733163ef7f44bf0535e62fa
KotoriMinami <chemingyu@foxmail.com>
2016-10-31 20:16:58 +0800

    no message

--- 01.PreResearch/src/com/ai/test/YeeTranslate.java
+++ 01.PreResearch/src/com/ai/test/YeeTranslate.java
@@ -8,7 +8,7 @@ public class YeeTranslate {
 	public static String TRANSREQ = "https://translateport.yeekit.com/translate";
 	
 	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException, IOException{
-		String paramter = "{\"srcl\":en,\"tgtl\":zn,\"text\":China,\"detoken\":true,\"align\":true}";
+		String paramter = "{\"srcl\":en,\"tgtl\":zh,\"text\":China,\"detoken\":true,\"align\":true}";
 		String resp = HttpsUtil.HttpsPost(TRANSREQ, paramter, "UTF-8");
 		System.out.println(resp);
 	}
