f284463018c3a033c0e461c7739fd4fabc2c800e
mnorange <icheng55@163.com>
2016-11-01 11:04:12 +0800

    机器翻译

--- 01.PreResearch/src/com/ai/test/YeeTranslate.java
+++ 01.PreResearch/src/com/ai/test/YeeTranslate.java
@@ -4,12 +4,25 @@ import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamWriterFactory.Zephyr;
+
+import net.sf.json.JSONObject;
+
 public class YeeTranslate {
 	public static String TRANSREQ = "https://translateport.yeekit.com/translate";
 	
 	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException, IOException{
-		String paramter = "{\"srcl\":en,\"tgtl\":zh,\"text\":China,\"detoken\":true,\"align\":true}";
-		String resp = HttpsUtil.HttpsPost(TRANSREQ, paramter, "UTF-8");
+//		String paramter = "{\"srcl\":zh,\"tgtl\":en,\"text\":%E4%B8%AD%E5%9B%BD,\"detoken\":true,\"align\":true}";
+		
+		JSONObject jsonObject=new JSONObject();
+		jsonObject.put("srcl", "zh");
+		jsonObject.put("tgtl", "en");
+		jsonObject.put("text", "你很漂亮");
+		jsonObject.put("detoken", true);
+		jsonObject.put("align", true);
+		
+		String resp = HttpsUtil.HttpsPost(TRANSREQ, jsonObject.toString(), "UTF-8");
 		System.out.println(resp);
 	}
 }
