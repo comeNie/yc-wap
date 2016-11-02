2d5fc6ae2b4aa00b9442ddc7632f846ee7d51c69
KotoriMinami <chemingyu@foxmail.com>
2016-11-02 11:32:35 +0800

    uodate intellij idea

---
+++ src/main/java/com/yc/wap/HomeController.java
@@ -0,0 +1,36 @@
+package com.yc.wap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		System.out.print("home");
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
