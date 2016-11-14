01b1fdebf4055d0852787d977f9f272c9a3f5afd
KotoriMinami <chemingyu@foxmail.com>
2016-11-14 16:35:20 +0800

    no message

---
+++ src/main/java/com/yc/wap/pay/PayController.java
@@ -0,0 +1,13 @@
+package com.yc.wap.pay;
+
+import com.yc.wap.system.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Nozomi on 11/14/2016.
+ */
+@Controller
+@RequestMapping(value = "pay")
+public class PayController extends BaseController{
+}
