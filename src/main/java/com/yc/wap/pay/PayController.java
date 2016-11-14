3e60f9ada837b6ef9e9f7eafa53d44cbc6cab79f
KotoriMinami <chemingyu@foxmail.com>
2016-11-14 17:38:02 +0800

    no message

--- src/main/java/com/yc/wap/pay/PayController.java
+++ src/main/java/com/yc/wap/pay/PayController.java
@@ -4,10 +4,17 @@ import com.yc.wap.system.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Nozomi on 11/14/2016.
  */
 @Controller
 @RequestMapping(value = "pay")
 public class PayController extends BaseController{
+    @RequestMapping(value = "/gotoPayByOrg")
+    public void gotoPayByOrg(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    }
 }
