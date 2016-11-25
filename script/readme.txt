87466fd00d2a6a13c721eb134fa28310e9758293
ldy <ldy@ldy.local>
2016-11-25 16:53:26 +0800

    no message

--- script/readme.txt
+++ script/readme.txt
@@ -14,14 +14,44 @@ docker push 10.19.13.20:5000/yc-wap-web:v1.0
 #--net=host  表示为主机(host)模式  去掉该配置，默认为桥接(bridge)模式
 #-e 代表需要设置的环境变量
 docker run -d --name yc-wap-web-v1.0  -p 14115:8080  \
--e "innerDomains=changhong.com" \
+-e "casServerLoginUrl=http://10.19.13.14:14125/uac/login"  \
+-e "casServerUrlPrefix=http://10.19.13.14:14125/uac"   \
+-e "serverName=http://localhost:8080"   \
+-e "logOutServerUrl=http://10.19.13.14:14125/uac/logout"   \
+-e "logOutBackUrl=http://10.19.13.14:14125/uac"   \
+-e "casServerLoginUrl_Inner=http://10.19.13.14:14125/uac/login"  \
+-e "casServerUrlPrefix_Inner=http://10.19.13.14:14125/uac"   \
+-e "serverName_Inner=http://localhost:8080"   \
+-e "logOutServerUrl_Inner=http://10.19.13.14:14125/uac/logout"   \
+-e "logOutBackUrl_Inner=http://10.19.13.14:14125/uac"   \
 -e "SDK_MODE=0" \
 -e "PAAS_AUTH_URL=http://10.1.245.4:19811/service-portal-uac-web/service/auth" \
 -e "PAAS_AUTH_PID=D14F7D708109471AB6F3084B2ABAE9A6" \
 -e "PAAS_CCS_ID=CCS011" \
 -e "PAAS_CCS_PWD=123456" \
 -e "REST_REGISTRY_ADDR=10.19.13.23:29181"  \
--e "whitelist=changhong.com" \
+-e "EMAIL_HOSTNAME=mail.asiainfo.com" \
+-e "EMAIL_FROM_MAIL=opt-common@asiainfo.com" \
+-e "EMAIL_FROM_NAME=译云" \
+-e "EMAIL_FROM_USER=opt-common@asiainfo.com" \
+-e "EMAIL_FROM_PASSWORD=1q2w3e4r@" \
+-e "TENANT_ID=yeecloud" \
+-e "REQUEST_KEY=7igfgyfwn2jlyjs3bdns6lhysnzy5y7l" \
+-e "ACTION_URL=http://10.19.13.24:14110/opt-pay/pay/gotoPayByOrg" \
+-e "APP_ACTION_URL=http://10.19.13.24:14110/opt-pay/pay/gotoPayByOrg" \
+-e "NOTIFY_URL=http://10.1.52.75:8080/pay/payResult" \
+-e "RETURN_URL=http://10.1.52.75:8080/pay/payResultView" \
+-e "SoaServerIpAddressA=10.19.13.29" \
+-e "SoaServerIpPortA=8082" \
+-e "HttpServerIpAddressA=10.19.13.29" \
+-e "HttpServerPortA=8082" \
+-e "TcpServerAddressA=10.19.13.29" \
+-e "TcpServerPortA=10086" \
+-e "TcpReceivePort=2011" \
+-e "soapnamespace=http://wsservice.sms.cc.order.com" \
+-e "yeecloudUser=yeecloud" \
+-e "yeecloudPassword=yee123" \
+-e "MessageFormYeecloud=yeecloud" \
 10.19.13.20:5000/yc-wap-web:v1.0
 
 #查看镜像启动日志
@@ -34,3 +64,4 @@ docker rm -fv yc-wap-web-v1.0
 #=============更新日志========================#
 *2016-09-23
 1）初始打包
+
--- script/start-web.sh
+++ script/start-web.sh
@@ -8,7 +8,7 @@ RESOURCES_HOME=${CATALINA_HOME}/webapps/ROOT/WEB-INF/classes
 #change config
 pushd ${RESOURCES_HOME}
 
-
+#paas-conf.properties
 sed -i "s%paas.auth.url=.*%paas.auth.url=${PAAS_AUTH_URL}%g" ./paas/paas-conf.properties
 sed -i "s%paas.auth.pid=.*%paas.auth.pid=${PAAS_AUTH_PID}%g" ./paas/paas-conf.properties
 sed -i "s%paas.ccs.serviceid=.*%paas.ccs.serviceid=${PAAS_CCS_ID}%g" ./paas/paas-conf.properties
@@ -20,6 +20,46 @@ sed -i "s%ccs.zk_address=.*%ccs.zk_address=${ZK_ADDR}%g" ./paas/paas-conf.proper
 
 # 各中心要根据情况自己修改成与dubbo.properties中对应的配置项
 sed -i "s%dubbo.registry.address=.*%dubbo.registry.address=${REST_REGISTRY_ADDR}%g" ./dubbo.properties
+
+# sso.properties
+sed -i "s%casServerLoginUrl=.*%casServerLoginUrl=${casServerLoginUrl}%g" ./sso.properties
+sed -i "s%casServerUrlPrefix=.*%casServerUrlPrefix=${casServerUrlPrefix}%g" ./sso.properties
+sed -i "s%serverName=.*%serverName=${serverName}%g" ./sso.properties
+sed -i "s%logOutServerUrl=.*%logOutServerUrl=${logOutServerUrl}%g" ./sso.properties
+sed -i "s%logOutBackUrl=.*%logOutBackUrl=${logOutBackUrl}%g" ./sso.properties
+sed -i "s%casServerLoginUrl_Inner=.*%casServerLoginUrl_Inner=${casServerLoginUrl_Inner}%g" ./sso.properties
+sed -i "s%casServerUrlPrefix_Inner=.*%casServerUrlPrefix_Inner=${casServerUrlPrefix_Inner}%g" ./sso.properties
+sed -i "s%serverName_Inner=.*%serverName_Inner=${serverName_Inner}%g" ./sso.properties
+sed -i "s%logOutServerUrl_Inner=.*%logOutServerUrl_Inner=${logOutServerUrl_Inner}%g" ./sso.properties
+sed -i "s%logOutBackUrl_Inner=.*%logOutBackUrl_Inner=${logOutBackUrl_Inner}%g" ./sso.properties
+
+#email-conf.properties
+sed -i "s%email.hostname=.*%email.hostname=${EMAIL_HOSTNAME}%g" ./email/email-conf.properties
+sed -i "s%email.from.mail=.*%email.from.mail=${EMAIL_FROM_MAIL}%g" ./email/email-conf.properties
+sed -i "s%email.from.name=.*%email.from.name=${EMAIL_FROM_NAME}%g" ./email/email-conf.properties
+sed -i "s%email.from.user=.*%email.from.user=${EMAIL_FROM_USER}%g" ./email/email-conf.properties
+sed -i "s%email.from.password=.*%email.from.password=${EMAIL_FROM_PASSWORD}%g" ./email/email-conf.properties
+
+#config.properties
+sed -i "s%TENANT_ID=.*%TENANT_ID=${TENANT_ID}%g" ./config.properties
+sed -i "s%REQUEST_KEY=.*%REQUEST_KEY=${REQUEST_KEY}%g" ./config.properties
+sed -i "s%ACTION_URL=.*%ACTION_URL=${ACTION_URL}%g" ./config.properties
+sed -i "s%APP_ACTION_URL=.*%APP_ACTION_URL=${APP_ACTION_URL}%g" ./config.properties
+sed -i "s%NOTIFY_URL=.*%NOTIFY_URL=${NOTIFY_URL}%g" ./config.properties
+sed -i "s%RETURN_URL=.*%RETURN_URL=${RETURN_URL}%g" ./config.properties
+
+#FocusSms.properties
+sed -i "s%SoaServerIpAddressA=.*%SoaServerIpAddressA=${SoaServerIpAddressA}%g" ./FocusSms.properties
+sed -i "s%SoaServerIpPortA=.*%SoaServerIpPortA=${SoaServerIpPortA}%g" ./FocusSms.properties
+sed -i "s%HttpServerIpAddressA=.*%HttpServerIpAddressA=${HttpServerIpAddressA}%g" ./FocusSms.properties
+sed -i "s%HttpServerPortA=.*%HttpServerPortA=${HttpServerPortA}%g" ./FocusSms.properties
+sed -i "s%TcpServerAddressA=.*%TcpServerAddressA=${TcpServerAddressA}%g" ./FocusSms.properties
+sed -i "s%TcpServerPortA=.*%TcpServerPortA=${TcpServerPortA}%g" ./FocusSms.properties
+sed -i "s%TcpReceivePort=.*%TcpReceivePort=${TcpReceivePort}%g" ./FocusSms.properties
+sed -i "s%soapnamespace=.*%soapnamespace=${soapnamespace}%g" ./FocusSms.properties
+sed -i "s%yeecloudUser=.*%yeecloudUser=${yeecloudUser}%g" ./FocusSms.properties
+sed -i "s%yeecloudPassword=.*%yeecloudPassword=${yeecloudPassword}%g" ./FocusSms.properties
+sed -i "s%MessageFormYeecloud=.*%MessageFormYeecloud=${MessageFormYeecloud}%g" ./FocusSms.properties
 popd
 
 
